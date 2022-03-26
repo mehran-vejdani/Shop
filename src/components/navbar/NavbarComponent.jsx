46fbe43349baa213207904bcc77b7e0d4fdab11d
mehran vejdani <mehran.vejdanii@gmail.com>
2022-03-26 15:44:58 +0430

    add navbarComponent&style

---
+++ src/components/navbar/NavbarComponent.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+//add material
+import styles from "./nav.module.css";
+import { Navbar, Container, Nav } from "react-bootstrap";
+import shop from "../assets/icons/shopping.svg";
+//end material
+const NavbarComponent = () => {
+  return (
+    <Navbar bg="light" expand="lg">
+      <Container>
+        <Link to="/products" className={styles.link}>
+          products
+        </Link>
+        <div>
+          <img src={shop} alt="shopIcon" className={styles.shopIcon} />
+          <span className={styles.shopCount}>0</span>
+        </div>
+      </Container>
+    </Navbar>
+  );
+};
+
+export default NavbarComponent;
